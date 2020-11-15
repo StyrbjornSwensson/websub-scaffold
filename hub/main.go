9cfcd61a8779c4e0e14dee316b6642b5f09786c4
Styrbjorn Swensson <styrbjorn.swensson@gmail.com>
2020-11-15 11:12:54 +0100

    docker compose working, hub receiving post request from client

--- hub/main.go
+++ hub/main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
 
-func main(){
-	fmt.Println("Hello Hub, implement me")
+import (
+	"encoding/json"
+	//"encoding/json"
+	"log"
+	"net/http"
+	//"math/rand"
+	//"strconv"
+	"github.com/gorilla/mux"
+)
+
+
+
+// create message slice
+var messages []string
+
+
+func recieveMessage(w http.ResponseWriter, r *http.Request) {
+ 	w.Header().Set("Content-Type", "application/json")
+	var message string
+	_ = json.NewDecoder(r.Body).Decode(&message)
+	messages = append(messages, message)
+}
+
+func getMessages(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(messages)
+}
+
+
+func main() {
+	// Init Router
+	router := mux.NewRouter()
+
+
+	// Route Handlers / Endpoints
+	router.HandleFunc("/", recieveMessage).Methods("POST")
+	router.HandleFunc("/", getMessages).Methods("GET")
+
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
