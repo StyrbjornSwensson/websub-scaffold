63340b79cc7f91ebce06e4091826d436cc684e19
Styrbjorn Swensson <styrbjorn.swensson@gmail.com>
2020-11-15 23:37:28 +0100

    200 response from client after confirmed subscription

--- hub/main.go
+++ hub/main.go
@@ -2,54 +2,92 @@ package main
 
 
 import (
-	"encoding/json"
+	"bytes"
+	"fmt"
 	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"time"
 )
 
-type FormValue struct {
-	key 	string	`json:"hub.callback"`
-	value []string	`json:"value"`
+
+type SubRequestResponse struct{
+	HubTopic	 	string 	`url:"hub.topic"`
+	HubMode 		string 	`url:"hub.mode"`
+	HubChallenge	string	`url:"hub.challenge"`
+	HubLease		string	`url:"hub.lease_seconds"`
 }
 
-type Message struct {
-	body string `json:"body"`
-}
 
-// create message slice
-var messages []Message
-var formvals []FormValue
-
-
-func receiveMessage(w http.ResponseWriter, r *http.Request) {
+func SubRequest(w http.ResponseWriter, r *http.Request) {
  	w.Header().Set("Content-Type", "application/json")
-	err := r.ParseForm()
-	var message Message
-	_ = json.NewDecoder(r.Body).Decode(message)
-	messages = append(messages, message)
+
+ 	buf := new(bytes.Buffer)
+ 	_, err := buf.ReadFrom(r.Body)
 
 	if err != nil {
 		log.Fatal(err)
-	}
-	for key, value := range r.PostForm {
-		var formval = FormValue{key, value}
-		formvals = append(formvals, formval)
+		return
 	}
 
+ 	newStr := buf.String()
+ 	decodedString, err := url.QueryUnescape(newStr)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	params, err := url.ParseQuery(decodedString)
+
+	subRequestResponse := SubRequestResponse{
+		HubTopic: params["hub.topic"][0],
+		HubMode: params["hub.mode"][0],
+		HubChallenge: createRandomString(),
+		HubLease: "3600",
+	}
+
+	confirmationParams := url.Values{}
+	confirmationParams.Add("hub.topic", subRequestResponse.HubTopic)
+	confirmationParams.Add("hub.mode", subRequestResponse.HubMode)
+	confirmationParams.Add("hub.challenge", subRequestResponse.HubChallenge)
+	confirmationParams.Add("hub.lease_seconds", subRequestResponse.HubLease)
+
+	u := &url.URL{
+		RawQuery: confirmationParams.Encode(),
+	}
+
+	subConfirmationResponse, err := http.Get(params["hub.callback"][0]+u.String())
+
+	fmt.Println(subConfirmationResponse)
+	fmt.Println(params["hub.callback"][0] + u.String())
 }
 
-func getMessages(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "application/json")
-	_= json.NewEncoder(w).Encode(messages)
+
+
+func createRandomString() string {
+
+		rand.Seed(time.Now().UnixNano())
+		digits := "0123456789"
+		specials := "~=+%^*/()[]{}/!@#$?|"
+		all := "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+			"abcdefghijklmnopqrstuvwxyz" +
+			digits + specials
+		length := 8
+		buf := make([]byte, length)
+		buf[0] = digits[rand.Intn(len(digits))]
+		buf[1] = specials[rand.Intn(len(specials))]
+		for i := 2; i < length; i++ {
+			buf[i] = all[rand.Intn(len(all))]
+		}
+		rand.Shuffle(len(buf), func(i, j int) {
+			buf[i], buf[j] = buf[j], buf[i]
+		})
+		str := string(buf)
+		return str
 }
-func getFormvals(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "application/json")
-	_= json.NewEncoder(w).Encode(formvals)
-}
-
-
-
 
 func main() {
 	// Init Router
@@ -57,9 +95,7 @@ func main() {
 
 
 	// Route Handlers / Endpoints
-	router.HandleFunc("/", receiveMessage).Methods("POST")
-	router.HandleFunc("/", getMessages).Methods("GET")
-	router.HandleFunc("/formvals/", getFormvals).Methods("GET")
+	router.HandleFunc("/", SubRequest).Methods("POST")
 
 
 	log.Fatal(http.ListenAndServe(":8080", router))
